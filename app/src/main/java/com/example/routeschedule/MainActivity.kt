b206d19d30fc8d802f2b57845c3a320477bc2250
Kirill Cherkas <kirill.cherkas34@gmail.com>
2022-06-13 10:42:41 +0300

    version1

--- app/src/main/java/com/example/routeschedule/MainActivity.kt
+++ app/src/main/java/com/example/routeschedule/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.routeschedule
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import com.example.routeschedule.presentation.mainScreen.MainFragment
+import com.example.routeschedule.presentation.openFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main,menu)
+        return true
+    }
+
+    override fun onStart() {
+        super.onStart()
+        openFragment(R.id.container,MainFragment.TAG,MainFragment.newInstance())
+        fdgfd
+    }
+
+
 }
